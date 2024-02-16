c1a9d26e886eb20d771caff5d984447b5f86df48
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2024-02-16 14:32:02 +0500

    Redirect Added

--- src/app/(main)/(routes)/(main)/page.tsx
+++ src/app/(main)/(routes)/(main)/page.tsx
@@ -1,7 +1,15 @@
 import StatsCards from "./_components/stats-cards";
 import ActiveUsers from "./_components/active-users";
+import { getServerAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const page = async () => {
+  const session = await getServerAuthSession();
+
+  if (session) {
+    console.log("REDIRECT FROM SIGN IN");
+    redirect("/");
+  }
   return (
     <>
       <StatsCards />
