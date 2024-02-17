dc0f4f6982c150673966ed137832cb2b275c6b64
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2024-02-17 14:37:29 +0500

    Re Direction Added

--- src/app/(main)/(routes)/(main)/page.tsx
+++ src/app/(main)/(routes)/(main)/page.tsx
@@ -1,14 +1,7 @@
 import StatsCards from "./_components/stats-cards";
 import ActiveUsers from "./_components/active-users";
-import { getServerAuthSession } from "@/lib/auth";
-import { redirect } from "next/navigation";
 
 const page = async () => {
-  const session = await getServerAuthSession();
-
-  if (!session) {
-    redirect("/sign-in");
-  }
   return (
     <>
       <StatsCards />
